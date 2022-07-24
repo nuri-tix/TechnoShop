d9e48891920937e60559f460a1903e54f348f4a8
nuri-tix <artweb1509@gmail.com>
2022-07-24 19:39:05 +0300

    Add Files

--- src/modules/menuFooter.js
+++ src/modules/menuFooter.js
@@ -1,15 +1,17 @@
 import { getCategory } from "./goodsService"
 
 export const menuFooter = () => {
-  const categoryFooter = document.querySelector('#category-footer')
+  const categoryFooter = document.querySelector('#category-footer');
+
   getCategory().then(categoryList => {
     for (const categoryListKey in categoryList) {
       // const point = document.createElement('li');
       const point = document.createElement('li');
-      point.className = 'footer__item';
+      point.className = 'footer__item';      
       point.value = categoryListKey;
       point.textContent = categoryList[categoryListKey];
       categoryFooter.append(point);
+      console.log(categoryFooter);
     }
   })
 }
